450cf461d89cc7da1002fc779ee8a4a8cd771f76
bogor <bogor@DESKTOP-DC6RO5J>
2021-12-12 18:13:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int i, a[3], min = INT_MAX;
+    cout << "Введите 3 числа: " << endl;
+    for (i = 0; i <= 2; i++)
+    {
+        cin >> a[i];
+    }
+    for (i = 0; i <= 2; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+
+    }
+    cout << "Минимальное число: " << min << endl;
+
+}
